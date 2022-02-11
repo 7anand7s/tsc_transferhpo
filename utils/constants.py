386d51bdb1a1dd6d55f222e3d31450103a334016
7anand7s <74720581+7anand7s@users.noreply.github.com>
2022-02-11 13:51:12 +0100

    Add files via upload

---
+++ utils/constants.py
@@ -0,0 +1,19 @@
+UNIVARIATE_DATASET_NAMES = ['Adiac', 'ChlorineConcentration', 'Cricket_X', 'Cricket_Y', 'Cricket_Z','DiatomSizeReduction',
+                            'DistalPhalanxOutlineCorrect', 'ECG5000', '50words', 'ArrowHead', 'Beef', 'BeetleFly',
+                            'BirdChicken', 'Car', 'CBF', 'DistalPhalanxOutlineAgeGroup', 'Earthquakes', 'ECG200',
+                            'DistalPhalanxTW', 'CinC_ECG_torso', 'Computers']
+
+
+UNIVARIATE_ARCHIVE_NAMES = ['TSC']
+
+SITS_DATASETS = ['SatelliteFull_TRAIN_c301', 'SatelliteFull_TRAIN_c200', 'SatelliteFull_TRAIN_c451',
+                 'SatelliteFull_TRAIN_c89', 'SatelliteFull_TRAIN_c677', 'SatelliteFull_TRAIN_c59',
+                 'SatelliteFull_TRAIN_c133']
+
+InlineSkateXPs_DATASETS = ['InlineSkate-32', 'InlineSkate-64', 'InlineSkate-128',
+                           'InlineSkate-256', 'InlineSkate-512', 'InlineSkate-1024',
+                           'InlineSkate-2048']
+
+dataset_names_for_archive = {'TSC': UNIVARIATE_DATASET_NAMES,
+                             'SITS': SITS_DATASETS,
+                             'InlineSkateXPs': InlineSkateXPs_DATASETS}
